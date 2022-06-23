7141c12d432e236911cca093c97debe6dd6e2e73
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-06-23 15:21:32 +0530

    Add files via upload

---
+++ ComplexNumber.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ComplexNumber {
+    int real, imaginary;
+    public ComplexNumber(int r, int i){
+        this.real = r;
+        this.imaginary = i;
+    }
+    public void ShowNum(){
+        System.out.println(this.real + " +i" + this.imaginary);
+    }
+    public static ComplexNumber add(ComplexNumber num1, ComplexNumber num2){
+        ComplexNumber res = new ComplexNumber(0, 0);
+        //adding real part
+        res.real = num1.real + num2.real;
+        //adding imaginary part
+        res.imaginary = num1.imaginary + num2.imaginary;
+
+        //returning the result
+        return res;
+    }
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int a, b, c, d;
+        System.out.println("Enter real part of 1st complex number:- ");
+        a = in.nextInt();
+        System.out.println("Enter imaginary part of 1st complex number:- ");
+        b = in.nextInt();
+        ComplexNumber com1 = new ComplexNumber(a,b);
+        System.out.println("First Complex Number:- ");
+        com1.ShowNum();
+        System.out.println("Enter real part of 2nd complex number:- ");
+        c = in.nextInt();
+        System.out.println("Enter imaginary part of 1st complex number:- ");
+        d = in.nextInt();
+        ComplexNumber com2 = new ComplexNumber(c,d);
+        System.out.println("2nd Complex Number:- ");
+        com2.ShowNum();
+
+        ComplexNumber res = add(com1, com2);
+
+        System.out.println("Addition of two complex number:- ");
+        res.ShowNum();
+    }
+}
