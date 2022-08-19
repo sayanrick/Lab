2a14c5d778a4c12077f3cb48a23c709e9cecf17f
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-08-19 11:42:45 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,99 @@
+package Lab_Session_18th_aug.com.book;
+
+public class Book {
+	private String bookId;
+	private String title;
+	private String author;
+	private String category;
+	private float price;
+	// Declaring parameterized constructor
+	public Book(String bookId, String title, String author, String category, float price) {
+		super();
+		try {
+			if((bookId.startsWith("B")) && (bookId.length()<=4)) {
+				this.bookId = bookId;
+			}
+
+			else {
+
+				throw new InvalidBookException("Invalid Data...");
+			}
+			this.title = title;
+			this.author = author;
+			if(category == "Science" || category == "Fiction" || category == "Technology" || category == "Others") {
+				this.category = category;
+			}
+
+			else {
+
+				throw new InvalidBookException("Invalid Data");
+			}
+
+			if(price>=0) {
+				this.price = price;
+			}
+			else {
+				throw new InvalidBookException("Invalid Data");
+			}
+
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	// getters and setters
+	public String getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(String bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		//		if(this.category.co))
+		this.category = category;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+
+	}
+
+
+	@Override
+	public String toString() {
+		return "Book [bookId=" + bookId + ", title=" + title + ", author=" + author + ", category=" + category
+				+ ", price=" + price + "]";
+	}
+
+
+}
+class InvalidBookException extends Exception {
+	public InvalidBookException(String s) {
+		super(s);
+	}
+}
