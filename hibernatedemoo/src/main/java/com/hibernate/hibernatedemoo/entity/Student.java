4e5af8efe03e7b57f1dc309e2ee6eafad10252aa
SayanRick <sayanrickdas@gmail.com>
2022-09-15 19:06:29 +0530

    Commit First
    Uploading files

---
+++ hibernatedemoo/src/main/java/com/hibernate/hibernatedemoo/entity/Student.java
@@ -0,0 +1,32 @@
+package com.hibernate.hibernatedemoo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="student_details")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(length = 20, nullable = false)
+	private String name;
+	@Column(length = 11, nullable = false, unique = true)
+	private long phone;
+	@Column(length = 20, nullable = false)
+	private String dept;
+	@Column(length = 20, nullable = false, unique = true)
+	private String email;
+}
