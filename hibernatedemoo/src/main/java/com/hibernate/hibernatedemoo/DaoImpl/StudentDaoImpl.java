3c58c8732f548b5e59d98486a0201369b4407080
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-09-16 12:36:30 +0530

    Update StudentDaoImpl.java

--- hibernatedemoo/src/main/java/com/hibernate/hibernatedemoo/DaoImpl/StudentDaoImpl.java
+++ hibernatedemoo/src/main/java/com/hibernate/hibernatedemoo/DaoImpl/StudentDaoImpl.java
@@ -36,11 +36,36 @@ public class StudentDaoImpl implements StudentDao{
 	}
 
 	public void updateStudent() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter ID: ");
+		int id = sc.nextInt();
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = session.beginTransaction();
+		
+		Student std = session.get(Student.class, id);
+		System.out.println("Enter Phne Number: ");
+		long phone = sc.nextLong();
+		std.setPhone(phone);
+		
+		session.update(std);
+		System.out.println("Updated Successfully...");
+		t.commit();
 		
 	}
 
 	public void deleteStudent() { 
-				
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter ID: ");
+		int id = sc.nextInt();
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction t = session.beginTransaction();
+		
+		Student std1 = session.get(Student.class, id);
+		session.delete(std1);
+		System.out.println("Deleted Successfully...");
+		t.commit();
 	}
 
 	public void fetchStudent() {
